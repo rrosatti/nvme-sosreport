ad4c3f4b9246ef7ea776726214004f400fe263f6
Rodrigo R. Galvão <rodrigo_rosatti@hotmail.com>
2017-05-24 15:03:49 -0300

    Make plugin works in RedHat
    The NvmePlugin class is responsible to get the info on /sys/block/ about the nvme devices whether the nvme-cli package is installed or not.
    It was implemented a separate class for RedHat and Debian plataforms. For now, the only one tested was the RedHatNvmePlugin class.
    
    The RedHatNvmePlugin class will "use" the add_copy_spec() method from NvmePlugin. It also check if the nvme-cli package is installed in the system, if so then it will pass some nvme commands to add_cmd_output() method.

--- nvme.py
+++ nvme.py
@@ -1,21 +1,62 @@
 # -*- coding: utf8 -*-
 # This is a sosreport plugin to collect configuration details and system information about NVMe devices
 
-from sos.plugins import Plugin, RedHatPlugin
+from sos.plugins import Plugin, RedHatPlugin, DebianPlugin
+import os
 
-class NvmePlugin(Plugin, RedHatPlugin):
+class NvmePlugin(Plugin):
     """Collect config and system information about NVMe devices"""
 
     plugin_name = "nvme"
-    packages = ('nvme-cli',)
-    
+    #packages = ('nvme-cli',) 
+    devices = [] 
+
+    def get_spec(self):  
+        i = 0
+	spec = []
+	while os.path.isdir('/sys/block/nvme%dn1' % i):
+	    self.devices.append("nvme%dn1" % i)
+    	    path = "/sys/block/nvme%dn1" % i
+    	    spec.extend([
+        	"%s/size" % path,
+        	"%s/dev" % path,
+        	"%s/wwid" % path,
+        	"%s/queue/hw_sector_size" % path,
+        	"%s/queue/logical_block_size" % path,
+        	"%s/queue/max_hw_sectors_kb" % path,
+        	"%s/queue/max_segments" % path,
+        	"%s/queue/max_segment_size" % path,
+        	"%s/queue/nr_requests" % path,
+        	"%s/queue/physical_block_size" % path,
+        	"%s/device/firmware_rev" % path,
+        	"%s/device/model" % path,
+        	"%s/device/serial" % path])
+    	    i += 1
+        return spec
+
     def setup(self):
-        self.add_copy_spec([
-	    "/sys/block/nvme1n1/device/firmware_rev",
-	    "/sys/block/nvme1n1/device/model"
-	])
+        spec = self.get_spec()
+	self.add_copy_spec(spec)
 
-	self.add_cmd_output("nvme list", root_symlink="nvme");
 	
-	
+class RedHatNvmePlugin(NvmePlugin, RedHatPlugin):
 
+    def setup(self):
+        super(RedHatNvmePlugin, self).setup()
+	ret = os.system("rpm -qa | grep nvme-cli")
+	if ret == 0:  
+	    for dev in self.devices:  
+	    	self.add_cmd_output([
+	        	"nvme list",
+	 		"nvme fw-log /dev/%s" % dev,
+			"nvme list-ctrl /dev/%s" % dev,
+			"nvme list-ns /dev/%s" % dev], root_symlink="nvme");
+
+
+class DebianNvmePlugin(NvmePlugin, DebianPlugin):
+
+    def setup(self):
+        super(DebianPlugin, self).setup()	
+	ret = os.system("dpkg -s nvme-cli")
+	if ret == 0:
+	    self.add_cmd_output("nvme list", root_symlink="nvme");
