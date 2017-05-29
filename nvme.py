122b47f2560d826c385acae04e10143df93dd640
Rodrigo R. Galvão <rodrigo_rosatti@hotmail.com>
2017-05-29 15:46:57 -0300

    Add new commands to add_cmd_output()
    Some commands were added to RedHatNvmePlugin class through add_cmd_output().

--- nvme.py
+++ nvme.py
@@ -2,7 +2,7 @@
 # This is a sosreport plugin to collect configuration details and system information about NVMe devices
 
 from sos.plugins import Plugin, RedHatPlugin, DebianPlugin
-import os
+import os, subprocess
 
 class NvmePlugin(Plugin):
     """Collect config and system information about NVMe devices"""
@@ -51,6 +51,31 @@ class RedHatNvmePlugin(NvmePlugin, RedHatPlugin):
 		self.add_cmd_output("nvme list-ns /dev/%s" % dev, suggest_filename="list-ns.%s" % dev)
 		self.add_cmd_output("nvme fw-log /dev/%s" % dev, suggest_filename="fw-log.%s" % dev)
 		self.add_cmd_output("nvme list-ctrl /dev/%s" % dev, suggest_filename="list-ctrl.%s" % dev)		
+		self.add_cmd_output("nvme id-ctrl -H /dev/%s" % dev, suggest_filename="id-ctrl.%s" % dev)
+		self.add_cmd_output("nvme id-ns -H /dev/%s" % dev, suggest_filename="id-ns.%s" % dev)
+		self.add_cmd_output("nvme smart-log /dev/%s" % dev, suggest_filename="smart-log.%s" % dev)
+		self.add_cmd_output("nvme error-log /dev/%s" % dev, suggest_filename="error-log.%s" % dev)
+		self.add_cmd_output("sh -c \"lsblk | grep %s | awk '{ print $4 }'\"" % dev, suggest_filename="block-size.%s" % dev)
+		
+		opal = subprocess.check_output("cat /proc/cpuinfo | grep firmware | grep OPAL | wc -l", shell=True)
+		opal = int(opal)
+		if opal == 1:
+		    grep_op = "mass-storage"
+		else:
+		    grep_op = "pci"
+	
+		# get info about slot location and pci location
+		slot = subprocess.check_output("lscfg -vl %s | grep nvme | grep %s | awk '{ print $4 }'" % (dev[0:-2], grep_op), shell=True).strip()
+		pci = subprocess.check_output("lscfg -vl %s | grep nvme | grep %s | awk '{ print $1 }'" % (dev[0:-2], grep_op), shell=True).strip()
+		
+		self.add_cmd_output("sh -c \"lscfg -vl %s | grep nvme | grep %s | awk '{ print $4 }'\"" % (dev[0:-2], grep_op), suggest_filename="slot_loc.%s" % dev)
+		self.add_cmd_output("sh -c \"lscfg -vl %s | grep nvme | grep %s | awk '{ print $1 }'\"" % (dev[0:-2], grep_op), suggest_filename="pci_loc.%s" % dev)
+		self.add_cmd_output("sh -c \"lspci -vs %s | grep 'Non-Volatile memory controller' | cut -c 46-\"" % pci.strip(), suggest_filename="pci_vdid.%s" % dev)
+		self.add_cmd_output("sh -c \"lspci -vs %s | grep Subsystem | cut -c 13-\"" % pci.strip(), suggest_filename="pci_ssid.%s" % dev)
+	
+	else:	    
+	    self.add_string_as_file("The nvme-cli tool is not installed. If you want more configuration details and system information about NVMe devices, you should install it.\n", "nvme-cli_not_installed")	
+
 
 class DebianNvmePlugin(NvmePlugin, DebianPlugin):
 
